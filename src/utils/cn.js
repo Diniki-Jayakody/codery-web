70a9cc6822b766a201d437ea49f072920037e78c
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2026-04-18 10:59:44 +0530

    Updates

--- src/utils/cn.js
+++
@@ -1,8 +0,0 @@
-// src/utils/cn.js
-export const cn = (...classes) =>
-    classes
-      .flatMap((c) => (typeof c === 'string' ? c.split(' ') : c))
-      .filter(Boolean)
-      .join(' ');
-  
-  export default cn;
